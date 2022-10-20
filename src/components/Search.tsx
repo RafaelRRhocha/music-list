d246accf4342e389f39b9e0db935df0f4a0c1dbd
Rafael <1rafaelrocha2@gmail.com>
2022-10-19 22:02:32 -0300

    feat: new form for search page finished

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,13 +1,26 @@
 import type { FC } from 'react';
+import { useState } from 'react';
 import Header from './Header';
 
 interface SearchProps {}
 
 const Search: FC<SearchProps> = ({}) => {
+  const [artist, setArtist] = useState('');
+
+  const getArtistName = (value: string) => setArtist(value);
+
   return (
     <div>
       <Header />
       <p>Tela de Search</p>
+      <form>
+        <input
+          value={ artist }
+          onChange={ ({ target: { value }}) => getArtistName(value)}
+          type="text"
+        />
+        <button disabled={ artist.length < 2 } type="button">Pesquisar</button>
+      </form>
     </div>
   );
 }
