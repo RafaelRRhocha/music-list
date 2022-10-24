0009de6488ed9f63b54f910a1be0bd8725135366
Rafael <1rafaelrocha2@gmail.com>
2022-10-24 20:38:43 -0300

    fix: next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'i.postimg.cc',
+      'user-images.githubusercontent.com',
+      'placeimg.com',
+      'img.icons8.com'
+    ],
+  },
 }
 
 module.exports = nextConfig
